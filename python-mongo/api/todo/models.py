ac0385b0d1d8b5d12e154c36b656bf2435a0776c
Wei Lim <weilim@microsoft.com>
2023-08-08 13:19:21 -0700

    simplify python app

--- python-mongo/api/todo/models.py
+++ python-mongo/api/todo/models.py
@@ -1,41 +1,15 @@
 from datetime import datetime
 from enum import Enum
 from typing import Optional
-
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
 from beanie import Document, PydanticObjectId
 from pydantic import BaseModel, BaseSettings
-
-def keyvault_name_as_attr(name: str) -> str:
-    return name.replace("-", "_").upper()
-
 
 class Settings(BaseSettings):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Load secrets from keyvault
-        if self.AZURE_KEY_VAULT_ENDPOINT:
-            credential = DefaultAzureCredential()
-            keyvault_client = SecretClient(self.AZURE_KEY_VAULT_ENDPOINT, credential)
-            for secret in keyvault_client.list_properties_of_secrets():
-                setattr(
-                    self,
-                    keyvault_name_as_attr(secret.name),
-                    keyvault_client.get_secret(secret.name).value,
-                )
-
-    AZURE_COSMOS_CONNECTION_STRING: str = ""
-    AZURE_COSMOS_DATABASE_NAME: str = "Todo"
-    AZURE_KEY_VAULT_ENDPOINT: Optional[str] = None
-    APPLICATIONINSIGHTS_CONNECTION_STRING: Optional[str] = None
-    APPLICATIONINSIGHTS_ROLENAME: Optional[str] = "API"
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
+    MONGODB_CONNECTION_STRING: str = ""
+    MONGODB_DATABASE_NAME: str = "Todo"
 
 class TodoList(Document):
     name: str
