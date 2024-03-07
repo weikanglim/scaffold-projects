fb494dfb30046650b9b1b0bb92eeb53a5b18c9b2
Wei Lim <weilim@microsoft.com>
2024-03-06 16:03:25 -0800

    add redis test

--- cat-app/app.py
+++ cat-app/app.py
@@ -1,8 +1,10 @@
 import os
 
 from flask import Flask, render_template, send_from_directory
+import redis
 
 app = Flask(__name__)
+r = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=os.getenv("REDIS_PORT", 6379), password=os.getenv("REDIS_PASSWORD"), db=0, decode_responses=True)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -11,7 +13,12 @@ def favicon():
 
 @app.route("/")
 def home():
+    r.incr("counter")
     return render_template("home.html")
+
+@app.route("/counter")
+def counter():
+    return r.get("counter")
 
 if __name__ == "__main__":
     app.run(debug=True)
